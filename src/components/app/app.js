cf3c9581d934967db74d07d61119c82b45985a61
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2024-11-29 16:32:07 +0300

    app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -47,17 +47,26 @@ class App extends Component{
     }
 
     onToggleIncrease = (id) => {
-        this.setState(({data}) => {
-            const index = data.findIndex(elem => elem.id === id);
+        // this.setState(({data}) => {
+        //     const index = data.findIndex(elem => elem.id === id);
 
-            const old = data[index];
-            const newItem = {...old, increase: !old.increase};
-            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+        //     const old = data[index];
+        //     const newItem = {...old, increase: !old.increase};
+        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
 
-            return {
-                data: newArr
-            }
-        })
+        //     return {
+        //         data: newArr
+        //     }            
+        // })
+        this.setState(({data})=>({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, increase: !item.increase}
+                }
+                return item;
+            })
+        }))
+
     }
 
     onToggleRise = (id) => {
